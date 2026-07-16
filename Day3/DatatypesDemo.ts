65705b5a63c8d8f75f272a70d2d465ec2eb60bac
Aravind Juloori <julooriaravind@gmail.com>
2026-07-16 08:15:02 +0530

    Pushing the code related to the DataTypes

--- Day3/DatatypesDemo.ts
+++ Day3/DatatypesDemo.ts
@@ -1,13 +1,10 @@
 /*
-JavaScript is a dynamically typed programming language and it is not type safety
-TypeScript is a statically typed programming language and it is type safety
+JavaScript is a dynamically typed programming language and its not type safety
+TypeScript is a statically typed programming language and its type safey
 
-DataTypes- it represents the what type of data we are storing in a variable 
+Data Type -It represents what type of data we are storing in a variable. 
 
-*/
-
-/* There are 2 types of data types
-1.Primitive data types
+1.Primitive data types -
 number
 String
 boolean
@@ -17,31 +14,29 @@ Any
 union type 
 void
 
-2.Non primitive data types
+
+2.non-primitive data types
 Array
 class
 Function
 Interface 
 Touple etc..
-
 */
 
 
+
 //1.Number type --- it represents both integers and floating point numbers
+/* let age:number=30;
+let price:number=46.90;
+let bignumber=12323213123;
 
-/* let age:number=25;
-let price=25.24;
-let big=4213123123;
 
+console.log("Age is:",age);
+console.log("Price is:",price);
+console.log("Big Number is:",bignumber); */
 
-console.log("Age:",age);
-console.log("price:",price);
-console.log("Big Number:",big);
-
-console.log(typeof(age))
-console.log(typeof(price))
- */
-
+// console.log(typeof(age));
+// console.log(typeof(price));
 
 
 //2.String Type-- it represents the textual data ,we can declare strings in 3 different types
@@ -53,15 +48,15 @@ console.log(typeof(price))
 
 */
 
-/* let firstname:string="John";
-let lastname:string='kenedy';
 
-// let greeting:string="Hello",firstname,lastname;
-// console.log("Hello",firstname,lastname);
+/* let firstname:string='John';
+let lastname:string="Doe";
+
+console.log("Hello",firstname,lastname);
 
 let greeting:string=`Hello ${firstname} ${lastname}`;
-console.log(greeting);
- */
+console.log(greeting); */
+
 
 //3.Boolean Type -- it respresents true/false  -- it will be used in conditional and looping statements
 
@@ -69,8 +64,8 @@ console.log(greeting);
 let hasJob:boolean=false;
 
 console.log("Is Student?",isStudent);
-console.log("Has Job?",hasJob); */
-
+console.log("Has Job?",hasJob);
+ */
 
 //4.Null and Undefined --special types for the absence of value
 
@@ -80,12 +75,13 @@ let notAssigned:undefined=undefined;
 console.log(emptyValue);
 console.log(notAssigned);
 
-let price:number;
+let price1:number;
 
-console.log(price);   //its compile time error */
+console.log(price1);   //its compile time error
+ */
 
+//5.Any-  We need to use this data type carefully as it will voilates the typescript type safety.
 
-//5.Any-  it will voilates the typescript benefits
 
 /* let value:any="welcome";
 console.log(typeof(value));
@@ -99,6 +95,7 @@ console.log(typeof(value))
 console.log(value);
  */
 
+
 //6.union type  -it will combine multiple types
 
 /* let id:number |string  | boolean;
@@ -109,18 +106,15 @@ id=12345;
 console.log(id);
 
 id=true;
-console.log(id)
-
- */
+console.log(id) */
 
 //7.void Type -It is used for the functions that don't return any value
 
-/* function show():void{
+function show():void{
     console.log("Welcome");
 }
 
-show(); */
-
+show();
 
 
 function sum(x:number,y:number):number{
